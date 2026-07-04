790b3d812803277106a232e008670520208d80ba
Raahat <raahatshaikh98@gmail.com>
2026-07-04 14:22:25 +0800

    Initial Commit

---
+++ ScreensApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreensApp
+//
+//  Created by Minnatun Ara Mihika on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreensApp/ScreensAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreensAppApp.swift
+//  ScreensApp
+//
+//  Created by Minnatun Ara Mihika on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreensAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
